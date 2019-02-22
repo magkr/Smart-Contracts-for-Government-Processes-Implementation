68c4cd2e737ead27b0591825cf8659bd8d96de28
Liv Hartoft Borre <livb@itu.dk>
2019-02-22 10:58:59 +0100

    initial setup with ganache and kovan networks

---
+++ truffle-config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://kovan.infura.io/${process.env.INFURA_API_KEY}`
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 42
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  }
+}
