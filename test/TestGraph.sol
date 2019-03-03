2b4561817d358dbe98c5143ac966c985f6e52fa2
magkr <magkr@itu.dk>
2019-03-03 13:58:05 +0100

    ready for test with .sol

--- test/TestGraph.sol
+++ test/TestGraph.sol
@@ -4,5 +4,20 @@ import "truffle/Assert.sol";
 import { Graph } from "../contracts/Graph.sol";
 
 contract TestGraph {
+  Graph.Digraph private _graph;
+
+  function testInit() public {
+    //Arrange
+    uint8 V = 0;
+
+    //Act
+    Graph.init(_graph);
+
+    //Assert
+    Assert.equal(_graph.V, V, "V should be 0 after init");
+  }
+
+
+
 
 }
