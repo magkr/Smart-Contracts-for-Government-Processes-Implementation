049fc618c491c1bbf8b2cb55ef94749fe578f343
Liv Hartoft Borre <livb@itu.dk>
2019-04-01 15:46:29 +0200

    ...

--- contracts/DataHandler.sol
+++ contracts/DataHandler.sol
@@ -1,8 +1,7 @@
 pragma solidity 0.5.0;
 
-import {CaseHandler} from './CaseHandler.sol';
 
-contract Graph is CaseHandler {
+contract Graph {
   /* enum DataType { INT, TEXT, FILE, BOOL } */
   enum NodeType { EXTRA, RESOLUTION, NORMAL }
 
@@ -43,47 +42,4 @@ contract Graph is CaseHandler {
     req[w].push(v);
   }
 
-
-  function getActions(uint caseID) public view returns (bytes32[] memory) {
-    require(caseID >= 0 && caseID <= cases.length);
-    bytes32[] memory toDo = new bytes32[](vxs.length);
-    uint count = 0;
-
-    for (uint v = 0; v < vxs.length; v++) {
-      if(_isReady(v, cases[caseID])) {
-        toDo[count] = vxs[v].title;
-        count++;
-      }
-    }
-
-    return _cut(toDo, count);
-  }
-
-  function _isReady(uint v, Case storage c) private view returns (bool) {
-    // if v doesnt exist, throw error
-    if (data[c.titleToID[vxs[v].title]].status == Status.DONE) return false;
-    for(uint j = 0; j < req[v].length; j++) {
-      uint reqIdx = req[v][j];
-      if (data[c.titleToID[vxs[reqIdx].title]].status != Status.DONE) return false;
-    }
-
-    return true;
-  }
-
-  function markData(uint32 dataID) public onlyOwnerOf(data[dataID].caseID)  {
-    /* TODO EXPLANATION AS PARAMETER */
-
-    data[dataID].status = Status.MARKED;
-    _cascade(data[dataID].name, cases[data[dataID].caseID]);
-  }
-
-  function _cascade(bytes32 _title, Case storage c) private {
-    for (uint i = 0; i < adj[_getIdx(_title)].length; i++) {
-      uint adjIdx = adj[_getIdx(_title)][i];
-      if (data[c.titleToID[vxs[adjIdx].title]].status == Status.DONE) {
-        data[c.titleToID[vxs[adjIdx].title]].status = Status.PENDING;
-        _cascade(vxs[adjIdx].title, c);
-      }
-    }
-  }
 }
